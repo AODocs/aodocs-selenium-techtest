ddc7fe357452788e9342a8dfd7b384c74da2448e
Raymond Burrowes Personal <raymond.burrowes@gmail.com>
2023-11-24 16:20:37 +0100

    t(): add validateUrlComplete()

--- src/test/java/validations/Compare.java
+++ src/test/java/validations/Compare.java
@@ -10,17 +10,29 @@ public class Compare {
         String protocol,
         String subDomain,
         String domainName,
-        String topLevelDomain
+        String topLevelDomain,
+        String pathName,
+        String queryCharacter,
+        String parameterStrings
         ){
-        String expUrl = protocol + "://" + subDomain + "." + domainName + "." + topLevelDomain;
+
+            String expUrl = protocol + "://" + subDomain + "." + domainName + "." + topLevelDomain;
         
+            Boolean result = false;
+            if(driver.getCurrentUrl().equalsIgnoreCase(expUrl)){
+                result = true;
+            }
+    
+            // Return both the comparison result & the exepected URL string constructed here
+            return new UrlResult(expUrl, result);
+    }
+
+    public static Boolean validateUrlComplete(WebDriver driver, String expUrl){
         Boolean result = false;
         if(driver.getCurrentUrl().equalsIgnoreCase(expUrl)){
             result = true;
         }
 
-        // Return both the comparison result & the exepected URL string constructed here
-        return new UrlResult(expUrl, result);
+        return result;
     }
-
 }
