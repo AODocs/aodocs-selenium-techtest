deb5820506495a3f4863c70fe25da18a8bc6ed6c
Raymond Burrowes Personal <raymond.burrowes@gmail.com>
2023-11-26 01:13:03 +0100

    t(): Remove details extra to Tech Test Reqs

--- src/test/java/tests/RequestAodocsDemo.java
+++ src/test/java/tests/RequestAodocsDemo.java
@@ -9,12 +9,10 @@ import functions.ClickAodocsWebElement;
 import functions.FillDemoRequestForm;
 import functions.GoogleSearch;
 import functions.OpenGoogleSearchResult;
-import functions.VisitWebpageDirect;
 import selenium.driver.Browser;
 import selenium.driver.WebDriverUtility;
 import utils.RandomGenerator;
 import validations.Errors;
-import validations.Urls;
 
 public class RequestAodocsDemo {
 
@@ -35,17 +33,10 @@ public class RequestAodocsDemo {
     }
     
     @Test
-    public void displayRequestForm(){
+    public void fillInRequestFormForTechTest(){
         String searchTextString = "aodocs";
         GoogleSearch.googleSearch(driver, searchTextString);
         OpenGoogleSearchResult.openAodocsResult(driver, searchTextString);
-        ClickAodocsWebElement.clickHomepageButton(driver, "Request a demo");
-        Urls.aodocsRequestDemo(driver, "https://www.aodocs.com/contact?request_type=request_demo");
-    }
-
-    @Test
-    public void fillInRequestFormForTechTest(){
-        VisitWebpageDirect.visitAodocsHomepage(driver);
         ClickAodocsWebElement.clickHomepageButton(driver, "Request a demo");
         String randomEmailString = RandomGenerator.generateRandomAlphanumericString10();
         FillDemoRequestForm.enterFirstName(driver, "Ray");
@@ -54,32 +45,5 @@ public class RequestAodocsDemo {
         FillDemoRequestForm.enterYourEmailAndTab(driver, randomEmailString);
         Errors.emailFormatIncorrect(driver);
         FillDemoRequestForm.selectCompanySize(driver, "0-4 employees");
-    }
-
-    @Test
-    public void fillInRequestFormTextFieldsOnly(){
-        
-    }
-
-    @Test
-    public void clickSubmitOnEmptyRequestForm(){
-        
-    }
-
-    @Test
-    public void sqlInjectionFirstNameField(){
-        VisitWebpageDirect.visitAodocsHomepage(driver);
-        ClickAodocsWebElement.clickHomepageButton(driver, "Request a demo");
-        FillDemoRequestForm.enterFirstName(driver, "SELECT * FROM Users WHERE UserId = 105 OR 1=1;");
-    }
-
-    @Test
-    public void invalidEmailFormat(){
-        
-    }
-
-    @Test
-    public void invalidPhoneNumber(){
-        
     }
 }
