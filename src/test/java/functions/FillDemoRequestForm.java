620f46a17933f480e6a93fffb1eef71c86cc82b0
Raymond Burrowes Personal <raymond.burrowes@gmail.com>
2023-11-27 10:46:09 +0100

    t(): remove used code & use css contains syntax

--- src/test/java/functions/FillDemoRequestForm.java
+++ src/test/java/functions/FillDemoRequestForm.java
@@ -3,78 +3,23 @@ package functions;
 import org.openqa.selenium.WebDriver;
 
 public class FillDemoRequestForm {
-    public static void completeFormForTechTest(
-        WebDriver driver,
-        String firstName,
-        String lastName,
-        String yourEmail,
-        String companySize
-        ){
-            enterFirstName(driver, firstName);
-            enterLastName(driver, lastName);
-            enterYourEmail(driver, yourEmail);
-            selectCompanySize(driver, companySize);
-    }
-
-    public static void completeTextFieldsOnly(
-        WebDriver driver,
-        String firstName,
-        String lastName,
-        String yourEmail,
-        String phoneNumber,
-        String yourCompany,
-        String yourMessage
-        ){
-            enterFirstName(driver, firstName);
-            enterLastName(driver, lastName);
-            enterYourCompany(driver, yourCompany);
-            enterYourEmail(driver, yourEmail);
-            enterPhoneNumber(driver, phoneNumber);
-            enterYourMessage(driver, yourMessage);
-    }
-
-    public static void enterFirstName(WebDriver driver, String firstName){
-        String firstNameSelector = "#firstname-384ed391-59a1-4016-bc91-62bb1307edb2_326";
-        EnterText.viaCssSelectors(driver, firstName, firstNameSelector);
-    }
-
-    public static void enterLastName(WebDriver driver, String lastName){
-        String lastNameSelector = "#lastname-384ed391-59a1-4016-bc91-62bb1307edb2_326";
-        EnterText.viaCssSelectors(driver, lastName, lastNameSelector);
+    public static void enterFirstNameAndTab(WebDriver driver, String firstName){
+        String firstNameSelector = "input[id*='firstname']";
+        EnterText.viaCssSelectorsAndTab(driver, firstName, firstNameSelector);
     }
 
     public static void enterLastNameAndTab(WebDriver driver, String lastName){
-        String lastNameSelector = "#lastname-384ed391-59a1-4016-bc91-62bb1307edb2_326";
+        String lastNameSelector = "input[id*='lastname']";
         EnterText.viaCssSelectorsAndTab(driver, lastName, lastNameSelector);
     }
 
-    public static void enterYourCompany(WebDriver driver, String yourCompany){
-        String yourCompanySelector = "#company-384ed391-59a1-4016-bc91-62bb1307edb2_326";
-        EnterText.viaCssSelectors(driver, yourCompany, yourCompanySelector);
-    }
-
     public static void selectCompanySize(WebDriver driver, String dropdownOption){
-        String companySizeSelector = "#company_size__c-384ed391-59a1-4016-bc91-62bb1307edb2_326";
+        String companySizeSelector = "select[id*='company_size']";
         MultiSelectOptionField.dropdownOptions(driver, companySizeSelector, dropdownOption);
     }
 
-    public static void enterYourEmail(WebDriver driver, String yourEmail){
-        String yourEmailSelector = "#email-384ed391-59a1-4016-bc91-62bb1307edb2_326";
-        EnterText.viaCssSelectors(driver, yourEmail, yourEmailSelector);
-    }
-
     public static void enterYourEmailAndTab(WebDriver driver, String yourEmail){
-        String yourEmailSelector = "#email-384ed391-59a1-4016-bc91-62bb1307edb2_326";
+        String yourEmailSelector = "input[id*='email']";
         EnterText.viaCssSelectorsAndTab(driver, yourEmail, yourEmailSelector);
-    }
-
-    public static void enterPhoneNumber(WebDriver driver, String phoneNumber){
-        String phoneNumberSelector = "#phone-384ed391-59a1-4016-bc91-62bb1307edb2_326";
-        EnterText.viaCssSelectors(driver, phoneNumber, phoneNumberSelector);
-    }
-
-    public static void enterYourMessage(WebDriver driver, String yourMessage){
-        String yourMessageSelector = "#your_request__c-384ed391-59a1-4016-bc91-62bb1307edb2_326";
-        EnterText.viaCssSelectors(driver, yourMessage, yourMessageSelector);
     }
 }
